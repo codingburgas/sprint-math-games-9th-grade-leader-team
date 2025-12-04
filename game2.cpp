7a98f172b22a6bc244073786839f582ab7370ee8
Olexsandra Kikavsyka <OOKikavsyka24@codingburgas.bg>
2025-12-04 13:28:38 +0200

    my second commit

---
+++ game2.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <clocale>    // для setlocale
+#ifdef _WIN32
+#include <windows.h>  // для SetConsoleCP / SetConsoleOutputCP
+#endif
+
+using namespace std;
+
+int main() {
+    // Для Windows: установить кодовую страницу UTF-8 в консоли (если нужно)
+    #ifdef _WIN32
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
+    #endif
+
+    // Настройка локали (для корректной обработки кириллицы)
+    setlocale(LC_ALL, "");
+
+    int score = 0;
+    int answer;
+
+    cout << "=============================\n";
+    cout << "     Викторина по програмиране\n";
+    cout << "           Игра на C++\n";
+    cout << "=============================\n\n";
+
+    cout << "Въпрос 1: Кой тип данни се използва за цели числа в C++?\n";
+    cout << "1) float\n";
+    cout << "2) int\n";
+    cout << "3) char\n";
+    cout << "Вашият отговор: ";
+    cin >> answer;
+    if (answer == 2) score++;
+
+    cout << "\nВъпрос 2: Кой оператор се използва за извеждане на текст в конзолата?\n";
+    cout << "1) cout <<\n";
+    cout << "2) cin >>\n";
+    cout << "3) print()\n";
+    cout << "Вашият отговор: ";
+    cin >> answer;
+    if (answer == 1) score++;
+
+    cout << "\nВъпрос 3: Как се нарича структура от инструкции, която се изпълнява при извикване?\n";
+    cout << "1) Променлива\n";
+    cout << "2) Клас\n";
+    cout << "3) Функция\n";
+    cout << "Вашият отговор: ";
+    cin >> answer;
+    if (answer == 3) score++;
+
+    cout << "\nВъпрос 4: Какво означава операторът == в C++?\n";
+    cout << "1) Присвояване на стойност\n";
+    cout << "2) Сравнение за равенство\n";
+    cout << "3) Спиране на програмата\n";
+    cout << "Вашият отговор: ";
+    cin >> answer;
+    if (answer == 2) score++;
+
+    cout << "=============================\n";
+    cout << "Вашият резултат: " << score << " от 4\n";
+    cout << "=============================\n";
+
+    return 0;
+}
